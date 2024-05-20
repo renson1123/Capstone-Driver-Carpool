98b9ee1577b060b7a05e75340a8760496baaaeaa
Renson Placino <rensplaci1123@gmail.com>
2024-05-20 16:00:37 +0800

    Adding Google Map and Getting Drivers Current Location

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../global/global_var.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -7,19 +16,65 @@ class MapPage extends StatefulWidget {
   State<MapPage> createState() => _MapPageState();
 }
 
-class _MapPageState extends State<MapPage> {
+class _MapPageState extends State<MapPage>
+{
+  final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
+  Position? currentPositionOfUser;
+
+
+  void updateMapTheme(GoogleMapController controller)
+  {
+    getJsonFileFromThemes("themes/night_style.json").then((value) => setGoogleMapStyle(value, controller));
+  }
+
+  Future<String> getJsonFileFromThemes(String mapStylePath) async
+  {
+    ByteData byteData = await rootBundle.load(mapStylePath);
+    var list = byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+    return utf8.decode(list);
+  }
+
+  setGoogleMapStyle(String googleMapStyle, GoogleMapController controller)
+  {
+    controller.setMapStyle(googleMapStyle);
+  }
+
+  getCurrentLiveLocationOfDriver() async
+  {
+    Position positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPositionOfUser = positionOfUser;
+
+    LatLng positionOfUserInLatLng = LatLng(currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+
+    CameraPosition cameraPosition = CameraPosition(target: positionOfUserInLatLng, zoom: 15);
+    controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Map",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
+    return Scaffold(
+      body: Stack(
+        children: [
 
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            initialCameraPosition: googlePlexInitialPosition,
+            onMapCreated: (GoogleMapController mapController)
+            {
+              controllerGoogleMap = mapController;
+
+              // customizing map style
+              // updateMapTheme(controllerGoogleMap!);
+
+              googleMapCompleterController.complete(controllerGoogleMap);
+
+              getCurrentLiveLocationOfDriver();
+            },
           ),
-        ),
+        ],
       ),
     );
   }
